cdcd3e95c1d6e08f8f04359f93e9dff753877e90
Ely De La Cruz <elycruz@elycruz.com>
2018-08-28 20:13:26 -0400

    chp8 - netcat example.

---
+++ chp8/netcat1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strconv"
+)
+
+func main ()  {
+	port := flag.Uint64("p", 8000, "Port number")
+
+	flag.Parse()
+
+	conn, err := net.Dial("tcp", "localhost:" +
+		strconv.FormatUint(*port, 10))
+
+	if err != nil { log.Fatal(err) }
+
+	defer conn.Close()
+
+	mustCopy(os.Stdout, conn)
+}
+
+func mustCopy (dest io.Writer, src io.Reader)  {
+	if _, err := io.Copy(dest, src); err != nil {
+		log.Fatal(err)
+	}
+}
