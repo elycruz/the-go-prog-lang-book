8a305ff126054fcdf8b049fd7f4328de5a8142f9
Ely De La Cruz <elycruz@elycruz.com>
2018-09-03 02:44:30 -0400

    chp8-pipelines - progress.

---
+++ chp8/pipelines/pipeline2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main () {
+	limit := flag.Int("l", 100, "Squares limit")
+	flag.Parse()
+	if *limit <= 0 {
+		fmt.Printf("Only positive limits allowed.  %d was given.\n", limit)
+		return
+	}
+
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	// Counter
+	go func () {
+		for x := 0 ; x <= *limit; x++ {
+			naturals <- x
+		}
+		close(naturals)
+	}()
+
+	// ETL
+	go func () {
+		for x := range naturals {
+			squares <- x * x
+		}
+		close(squares)
+	}()
+
+	for x := range squares {
+		fmt.Println(x)
+	}
+}
